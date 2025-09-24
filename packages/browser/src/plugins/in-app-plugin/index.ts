d354b0a79f63860dc08c90757e9cbd93f714d616
Bernard Gatt <bernard@bourbon.sh>
2025-09-24 14:44:45 +0200

    Setting cio_anonymous_id custom attribute on load and reset (#64)
    * Fixes linting problems
    
    * Added setAnonymousId
    Updated to latest Gist library
    Added tests
    
    ---------
    
    Co-authored-by: Bernard Gatt <bernard@customer.io>

--- packages/browser/src/plugins/in-app-plugin/index.ts
+++ packages/browser/src/plugins/in-app-plugin/index.ts
@@ -2,208 +2,240 @@ import { Analytics } from '../../core/analytics'
 import { Context } from '../../core/context'
 import { Plugin } from '../../core/plugin'
 
-import { InAppEvents, JourneysEvents, newEvent, allEvents, gistToCIO, ContentType } from './events'
+import {
+  InAppEvents,
+  JourneysEvents,
+  newEvent,
+  allEvents,
+  gistToCIO,
+  ContentType,
+} from './events'
 import Gist from 'customerio-gist-web'
 
 export { InAppEvents }
 
 export type InAppPluginSettings = {
-    siteId: string | undefined
-    events: EventListenerOrEventListenerObject | null | undefined
+  siteId: string | undefined
+  events: EventListenerOrEventListenerObject | null | undefined
 
-    _env: string | undefined
-    _logging: boolean | undefined
+  _env: string | undefined
+  _logging: boolean | undefined
 
-    anonymousInApp: boolean | false
+  anonymousInApp: boolean | false
 }
 
-export function InAppPlugin(
-    settings: InAppPluginSettings,
-  ): Plugin {
+export function InAppPlugin(settings: InAppPluginSettings): Plugin {
+  let _analytics: Analytics
+  let _gistLoaded = false
+  let _pluginLoaded = false
+  const _eventTarget: EventTarget = new EventTarget()
 
-    let _analytics: Analytics;
-    let _gistLoaded:boolean = false;
-    let _pluginLoaded:boolean = false;
-    const _eventTarget:EventTarget = new EventTarget();
+  async function setAnonymousId() {
+    const anonymousId = _analytics.user().anonymousId()
+    if (anonymousId) {
+      await Gist.setCustomAttribute('cio_anonymous_id', anonymousId)
+    }
+  }
 
-    function attachListeners() {
-        if(!_gistLoaded || _pluginLoaded)
-            return;
+  function attachListeners() {
+    if (!_gistLoaded || _pluginLoaded) return
 
-        _analytics.on('reset', reset);
+    _analytics.on('reset', reset)
 
-        if(settings.events) {
-            allEvents.forEach((event) => {
-                _eventTarget.addEventListener(event, settings?.events as EventListenerOrEventListenerObject);
-            });
-            ['messageDismissed', 'messageError'].forEach((event) => {
-                Gist.events.on(event, (message: any) => {
-                    _eventTarget.dispatchEvent(newEvent(gistToCIO(event), { 
-                        messageId: message.messageId,
-                        deliveryId: message.properties?.gist?.campaignId,
-                    }));
-                });
-            });
+    if (settings.events) {
+      allEvents.forEach((event) => {
+        _eventTarget.addEventListener(
+          event,
+          settings?.events as EventListenerOrEventListenerObject
+        )
+      })
+      ;['messageDismissed', 'messageError'].forEach((event) => {
+        Gist.events.on(event, (message: any) => {
+          _eventTarget.dispatchEvent(
+            newEvent(gistToCIO(event), {
+              messageId: message.messageId,
+              deliveryId: message.properties?.gist?.campaignId,
+            })
+          )
+        })
+      })
+    }
+
+    Gist.events.on('messageShown', (message: any) => {
+      const deliveryId: string = message?.properties?.gist?.campaignId
+      if (settings.events) {
+        _eventTarget.dispatchEvent(
+          newEvent(InAppEvents.MessageOpened, {
+            messageId: message?.messageId,
+            deliveryId: deliveryId,
+            message: {
+              dismiss: function () {
+                Gist.dismissMessage(message?.instanceId)
+              },
+            },
+          })
+        )
+      }
+      if (typeof deliveryId !== 'undefined' && deliveryId !== '') {
+        void _analytics.track(JourneysEvents.Metric, {
+          deliveryId: deliveryId,
+          metric: JourneysEvents.Opened,
+        })
+        return
+      }
+      const broadcastId: Number =
+        message?.properties?.gist?.broadcast?.broadcastIdInt
+      if (broadcastId) {
+        const templateId = message?.properties?.gist?.broadcast?.templateId
+        void _analytics.track(JourneysEvents.Content, {
+          actionType: JourneysEvents.ViewedContent,
+          contentId: broadcastId,
+          templateId: templateId,
+          contentType: ContentType,
+        })
+      }
+    })
+
+    Gist.events.on('messageAction', (params: any) => {
+      const deliveryId: string = params?.message?.properties?.gist?.campaignId
+      if (settings.events) {
+        _eventTarget.dispatchEvent(
+          newEvent(InAppEvents.MessageAction, {
+            messageId: params.message.messageId,
+            deliveryId: deliveryId,
+            action: params.action,
+            name: params.name,
+            actionName: params.name,
+            actionValue: params.action,
+            message: {
+              dismiss: function () {
+                Gist.dismissMessage(params.message.instanceId)
+              },
+            },
+          })
+        )
+      }
+      if (params.action === 'gist://close') {
+        return
+      }
+      if (typeof deliveryId !== 'undefined' && deliveryId !== '') {
+        void _analytics.track(JourneysEvents.Metric, {
+          deliveryId: deliveryId,
+          metric: JourneysEvents.Clicked,
+          actionName: params.name,
+          actionValue: params.action,
+        })
+        return
+      }
+      const broadcastId: Number =
+        params?.message?.properties?.gist?.broadcast?.broadcastIdInt
+      if (broadcastId) {
+        const templateId: Number =
+          params?.message?.properties?.gist?.broadcast?.templateId
+        void _analytics.track(JourneysEvents.Content, {
+          actionType: JourneysEvents.ClickedContent,
+          contentId: broadcastId,
+          templateId: templateId,
+          contentType: ContentType,
+          actionName: params.name,
+          actionValue: params.action,
+        })
+      }
+    })
+
+    Gist.events.on('eventDispatched', (gistEvent: any) => {
+      if (gistEvent.name === 'analytics:track') {
+        const trackEventName: string = gistEvent.payload?.event
+        if (typeof trackEventName === 'undefined' || trackEventName === '') {
+          return
         }
+        void _analytics.track(
+          trackEventName,
+          gistEvent.payload?.properties,
+          gistEvent.payload?.options
+        )
+      }
+    })
+  }
 
-        Gist.events.on('messageShown', (message: any) => {
-            const deliveryId:string = message?.properties?.gist?.campaignId;
-            if (settings.events) {
-                _eventTarget.dispatchEvent(newEvent(InAppEvents.MessageOpened, { 
-                    messageId: message?.messageId,
-                    deliveryId: deliveryId,
-                    message: {
-                        dismiss: function() {
-                            Gist.dismissMessage(message?.instanceId);
-                        }
-                    }
-                }));
-            }
-            if (typeof deliveryId != 'undefined' && deliveryId != '') {
-                _analytics.track(JourneysEvents.Metric, {
-                    'deliveryId': deliveryId,
-                    'metric': JourneysEvents.Opened,
-                });
-                return;
-            }
-            const broadcastId:Number = message?.properties?.gist?.broadcast?.broadcastIdInt;
-            if (broadcastId) {
-                const templateId = message?.properties?.gist?.broadcast?.templateId;
-                _analytics.track(JourneysEvents.Content, {
-                    'actionType': JourneysEvents.ViewedContent,
-                    'contentId': broadcastId,
-                    'templateId': templateId,
-                    'contentType': ContentType,
-                });
-            }
-        });
+  function page(ctx: Context): Context {
+    if (!_pluginLoaded) return ctx
 
-        Gist.events.on('messageAction', (params: any) => {
-            const deliveryId:string = params?.message?.properties?.gist?.campaignId;
-            if (settings.events) {
-                _eventTarget.dispatchEvent(newEvent(InAppEvents.MessageAction, {
-                    messageId: params.message.messageId,
-                    deliveryId: deliveryId,
-                    action: params.action,
-                    name: params.name,
-                    actionName: params.name,
-                    actionValue: params.action,
-                    message: {
-                        dismiss: function() {
-                            Gist.dismissMessage(params.message.instanceId);
-                        }
-                    }
-                }));
-            }
-            if (params.action == 'gist://close') {
-                return;
-            }
-            if (typeof deliveryId != 'undefined' && deliveryId != '') {
-                _analytics.track(JourneysEvents.Metric, {
-                    'deliveryId': deliveryId,
-                    'metric': JourneysEvents.Clicked,
-                    'actionName': params.name,
-                    'actionValue': params.action,
-                });
-                return;
-            }
-            const broadcastId:Number = params?.message?.properties?.gist?.broadcast?.broadcastIdInt;
-            if (broadcastId) {
-                const templateId:Number = params?.message?.properties?.gist?.broadcast?.templateId;
-                _analytics.track(JourneysEvents.Content, {
-                    'actionType': JourneysEvents.ClickedContent,
-                    'contentId': broadcastId,
-                    'templateId': templateId,
-                    'contentType': ContentType,
-                    'actionName': params.name,
-                    'actionValue': params.action,
-                });
-            }
-        });
-
-        Gist.events.on('eventDispatched', (gistEvent: any) => {
-            if(gistEvent.name == 'analytics:track') {
-                const trackEventName:string = gistEvent.payload?.event;
-                if(typeof trackEventName === 'undefined' || trackEventName == '') {
-                    return;
-                }
-                _analytics.track(trackEventName, gistEvent.payload?.properties, gistEvent.payload?.options);
-            }
-        });
+    const page: string =
+      ctx.event?.properties?.name ?? ctx.event?.properties?.url
+    if (typeof page === 'string' && page.length > 0) {
+      Gist.setCurrentRoute(page)
     }
 
-    async function page(ctx: Context): Promise<Context> {
-        if(!_pluginLoaded)
-            return ctx;
+    return ctx
+  }
 
-        const page:string = ctx.event?.properties?.name ?? ctx.event?.properties?.url;
-        if(typeof page === 'string' && page.length > 0) {
-            Gist.setCurrentRoute(page);
-        }
+  async function reset(ctx: Context): Promise<Context> {
+    await Gist.clearUserToken()
+    await Gist.clearCustomAttributes()
+    await setAnonymousId()
+    return ctx
+  }
 
-        return ctx;
+  async function syncUserToken(ctx: Context): Promise<Context> {
+    if (!_gistLoaded) return ctx
+
+    const user = _analytics.user().id()
+    if (typeof user === 'string' && user.length > 0) {
+      await Gist.setUserToken(user)
+    } else {
+      await Gist.clearUserToken()
     }
+    return ctx
+  }
 
-    async function reset(ctx: Context): Promise<Context> {
-        await Gist.clearUserToken();
-        return ctx;
-    }
-    
-    async function syncUserToken(ctx: Context): Promise<Context> {
-        if(!_gistLoaded)
-            return ctx;
+  const customerio: Plugin = {
+    name: 'Customer.io In-App Plugin',
+    type: 'before',
+    version: '0.0.1',
+    isLoaded: (): boolean => _pluginLoaded,
+    load: async (ctx: Context, instance: Analytics) => {
+      _analytics = instance
 
-        const user = _analytics.user().id();
-        if (typeof user === 'string' && user.length > 0) {
-            await Gist.setUserToken(user);
-        } else {
-            await Gist.clearUserToken();
-        }
-        return ctx;
-    }
+      if (settings.siteId == null || settings.siteId === '') {
+        _error("siteId is required. Can't initialize.")
+        return ctx
+      }
 
-    const customerio: Plugin = {
-        name: 'Customer.io In-App Plugin',
-        type: 'before',
-        version: '0.0.1',
-        isLoaded: (): boolean => _pluginLoaded,
-        load: async (ctx: Context, instance: Analytics) => {
-            _analytics = instance;
+      await setAnonymousId()
 
-            if(settings.siteId == null || settings.siteId == "") {
-                _error("siteId is required. Can't initialize.")
-                return ctx;
-            }
+      await Gist.setup({
+        siteId: settings.siteId,
+        env: settings._env ? settings._env : 'prod',
+        logging: settings._logging,
+        useAnonymousSession: settings.anonymousInApp,
+      })
+      _gistLoaded = true
 
-            await Gist.setup({
-                siteId: settings.siteId, 
-                env: settings._env? settings._env : "prod",
-                logging: settings._logging,
-                useAnonymousSession: settings.anonymousInApp,
-            });
-            _gistLoaded = true;
+      await syncUserToken(ctx)
+      attachListeners()
 
-            await syncUserToken(ctx);
-            attachListeners();
+      _pluginLoaded = true
 
-            _pluginLoaded = true;
+      return Promise.resolve()
+    },
+    identify: syncUserToken,
+    page: page,
+    unload: () => {
+      if (settings.events) {
+        allEvents.forEach((event) => {
+          _eventTarget.removeEventListener(
+            event,
+            settings?.events as EventListenerOrEventListenerObject
+          )
+        })
+      }
+    },
+  }
 
-            return Promise.resolve();
-        },
-        identify: syncUserToken,
-        page: page,
-        unload: async () => {
-            if(settings.events) {
-                allEvents.forEach((event) => {
-                    _eventTarget.removeEventListener(event, settings?.events as EventListenerOrEventListenerObject);
-                });
-            }
-        },
-    }
-
-  return customerio;
+  return customerio
 }
 
 function _error(msg: string) {
-    console.error(`[Customer.io In-App Plugin] ${msg}`)
+  console.error(`[Customer.io In-App Plugin] ${msg}`)
 }
