6222aa4029d4255738762cd3194bb3b9680a7c7d
Richard Dawe <rich.dawe@customer.io>
2025-10-16 09:35:32 +0100

    Mise tasks to improve local DX

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -4,4 +4,9 @@
 find . \( -name ".turbo" -o -name "dist" -o -name ".next" -o -name "tsconfig.tsbuildinfo" \) ! -path "*/node_modules/*" -print0 | xargs -0 rm -rf
 rm -rf node_modules/.cache
 
+rm -f package.tgz
+for dir in core browser node; do
+    rm -f packages/$dir/package.tgz
+done
+
 echo "Build files and cache deleted."
